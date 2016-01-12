0cea849ad7e67b29d460a302ab67436cb2cefe5f
Caue Beloni <cbeloni@gmail.com>
2016-01-11 21:35:49 -0300

    Alterado nome classe grafico

--- src/com/appgraph/view/GraficoBean.java
+++ src/com/appgraph/view/GraficoBean.java
@@ -1,20 +1,21 @@
 package com.appgraph.view;
 
 import javax.annotation.PostConstruct;
+
 import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
- 
+
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
 import org.primefaces.model.chart.BarChartModel;
-import org.primefaces.model.chart.HorizontalBarChartModel;
 import org.primefaces.model.chart.ChartSeries;
  
+@SuppressWarnings("serial")
 @ManagedBean
-public class Grafico implements Serializable {
+public class GraficoBean implements Serializable {
  
     private BarChartModel barModel;
-    private HorizontalBarChartModel horizontalBarModel;
  
     @PostConstruct
     public void init() {
@@ -23,11 +24,7 @@ public class Grafico implements Serializable {
  
     public BarChartModel getBarModel() {
         return barModel;
-    }
-     
-    public HorizontalBarChartModel getHorizontalBarModel() {
-        return horizontalBarModel;
-    }
+    }    
  
     private BarChartModel initBarModel() {
         BarChartModel model = new BarChartModel();
@@ -56,7 +53,6 @@ public class Grafico implements Serializable {
      
     private void createBarModels() {
         createBarModel();
-        createHorizontalBarModel();
     }
      
     private void createBarModel() {
@@ -73,40 +69,6 @@ public class Grafico implements Serializable {
         yAxis.setMin(0);
         yAxis.setMax(200);
     }
-     
-    private void createHorizontalBarModel() {
-        horizontalBarModel = new HorizontalBarChartModel();
- 
-        ChartSeries boys = new ChartSeries();
-        boys.setLabel("Boys");
-        boys.set("2004", 50);
-        boys.set("2005", 96);
-        boys.set("2006", 44);
-        boys.set("2007", 55);
-        boys.set("2008", 25);
- 
-        ChartSeries girls = new ChartSeries();
-        girls.setLabel("Girls");
-        girls.set("2004", 52);
-        girls.set("2005", 60);
-        girls.set("2006", 82);
-        girls.set("2007", 35);
-        girls.set("2008", 120);
- 
-        horizontalBarModel.addSeries(boys);
-        horizontalBarModel.addSeries(girls);
-         
-        horizontalBarModel.setTitle("Horizontal and Stacked");
-        horizontalBarModel.setLegendPosition("e");
-        horizontalBarModel.setStacked(true);
-         
-        Axis xAxis = horizontalBarModel.getAxis(AxisType.X);
-        xAxis.setLabel("Births");
-        xAxis.setMin(0);
-        xAxis.setMax(200);
-         
-        Axis yAxis = horizontalBarModel.getAxis(AxisType.Y);
-        yAxis.setLabel("Gender");        
-    }
+        
  
 }
